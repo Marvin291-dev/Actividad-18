a8433db6b4ada5dafa50c6f9cdab88c7ddd26418
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-09-06 00:03:17 -0600

    bandas

---
+++ logica.py
@@ -0,0 +1,6 @@
+class Registro:
+    def __init__(self, ventana_principal):
+        self.ventana_principal = ventana_principal
+
+def registrarpuntajesporcriterios():
+    print('registrarpuntajesporcriterios')
